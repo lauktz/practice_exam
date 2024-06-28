e56321db5738f2d9b31cfd85ffe0fc144a69cd06
laura-katz <laura.katz99@gmail.com>
2024-06-28 03:37:47 +0300

    added try-except

--- app.py
+++ app.py
@@ -3,24 +3,27 @@ from json_helper import read_json
 
 # cars = [{"model": 2000, "brand": "Kia", "color": "blue"}, {"model": 2001, "brand": "bmw", "color": "silver"}]
 
-
-
-def menu(cars):
-    while True:
-        print("Laura's Garage")
-        print('Choose one of the following actions: ')
-        print("1 - Get all cars")
-        print("2 - Add car")
-        print("3 - Delete car")
-        print("4 - Edit car")
-        print("5 - Save cars")
-        print("6 - Exit")
-        choice = input("")
-        menu_selection(cars,choice)
+try:
+    def menu(cars):
+        while True:
+            print("Laura's Garage")
+            print('Choose one of the following actions: ')
+            print("1 - Get all cars")
+            print("2 - Add car")
+            print("3 - Delete car")
+            print("4 - Edit car")
+            print("5 - Save cars")
+            print("6 - Exit")
+            choice = input("")
+            menu_selection(cars,choice)
  
-def main():
-    cars = read_json();
-    menu(cars)
+    def main():
+        cars = read_json();
+        menu(cars)
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
+
+
+except Exception as err:
+    print(err)
